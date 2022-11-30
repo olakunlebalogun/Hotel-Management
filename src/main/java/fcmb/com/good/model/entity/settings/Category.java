2b4cf2acee06c25bf70ee607cd79be0ecf9dd164
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-11-30 17:55:51 +0100

    closes #2;  imported some files

--- src/main/java/fcmb/com/good/model/entity/settings/Category.java
+++ src/main/java/fcmb/com/good/model/entity/settings/Category.java
@@ -1,5 +1,8 @@
 package fcmb.com.good.model.entity.settings;
 
+import fcmb.com.good.model.entity.settings.enums.Currency;
+import fcmb.com.good.model.entity.settings.enums.Department;
+
 public class Category {
     private String designation;
     private Department department;
