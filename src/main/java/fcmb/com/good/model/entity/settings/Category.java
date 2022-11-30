e0cf010c5bbae353c9f1bae5945bf8339f5f6efc
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-11-30 17:52:30 +0100

    closed #2, added the setting entity and its sub-entities

---
+++ src/main/java/fcmb/com/good/model/entity/settings/Category.java
@@ -0,0 +1,7 @@
+package fcmb.com.good.model.entity.settings;
+
+public class Category {
+    private String designation;
+    private Department department;
+    private Currency currency;
+}
---
+++ src/main/java/fcmb/com/good/model/entity/settings/Settings.java
@@ -0,0 +1,12 @@
+package fcmb.com.good.model.entity.settings;
+
+import fcmb.com.good.model.entity.BaseEntity;
+
+public class Settings extends BaseEntity {
+
+    private String description;
+    private Category category;
+    private String postedBy;
+
+
+}
