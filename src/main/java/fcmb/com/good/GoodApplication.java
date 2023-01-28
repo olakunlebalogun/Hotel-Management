3da807d059be88a687b20c63acc416945c5dc0e0
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-01-28 19:45:26 +0100

    did some basic clean-ups

--- src/main/java/fcmb/com/good/GoodApplication.java
+++ src/main/java/fcmb/com/good/GoodApplication.java
@@ -16,31 +16,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @PropertySource(value = "file: /home/olakunle/Folders/spring/Collaborations/config_files/application.properties", ignoreResourceNotFound = true  )
 public class GoodApplication {
 
-  /*  @PostConstruct
-    public void initUsers() {
-        List<User> users = Stream.of(
-                new User( "emmy", "emmytemmy@gmail.com", "N0 3 raye street","emmytemmy","emmy"),
-                new User( "emmy", "emmytemmy@gmail.com", "N0 3 raye street","user1","pwd1")
-                ).collect(Collectors.toList());
-        userRepository.saveAll(users);
-    }*/
-
 	public static void main(String[] args) {
 
         SpringApplication.run(GoodApplication.class, args);
 	}
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
-//            }
-//        };
-//    }
-
-
-
 
 }
